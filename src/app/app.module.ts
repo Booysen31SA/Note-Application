5ac8f6afa9fa39f8290d1c0a33b077f2ae446af6
Matthew Booysen <MatthewBoo@awards.co.za>
2019-11-12 15:54:18 +0200

    login form and top-bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    TopBarComponent,
   ],
   imports: [
     BrowserModule,
