26fb9e7ab3def674f41949679557f7820fa06da6
Matthew Booysen <MatthewBoo@awards.co.za>
2019-11-22 09:39:52 +0200

    added api service to registration

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { APIServiceService} from '../Services/apiservice.service';
 
 @Component({
   selector: 'app-registration',
@@ -8,7 +9,7 @@ import {Router} from '@angular/router';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor( private router: Router) { }
+  constructor( private router: Router, private apiService: APIServiceService) { }
 
   ngOnInit() {
   }
